9b02340f0b4a1b8b9f5ea9a61bb6c40d6a3ebdbe
Ola Abdallah <67606039+OlaAbdallah1@users.noreply.github.com>
2022-07-22 21:38:29 +0300

    Add files via upload

---
+++ routes/front.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const request = require ('request')
+const router = express.Router()
+
+router.use('/books/info/:id', async(req, res)=>{
+    const id = req.params.id
+    const url = 'http://192.168.56.102/books/info/'+id
+    request({url, json:true}, (error,  {body})=>{
+           if(error){
+               return res.staus(404).send(error)
+           }
+           return res.status(200).send(body)
+        })
+})
+router.use('/books/search/:topic', (req, res)=>{
+    const topic = req.params.topic
+    const url = 'http://192.168.56.102/books/search/'+topic
+    request({url, json:true}, (error,  {body})=>{
+           if(error){
+               return res.staus(404).send(error)
+           }
+           return res.staus(200).send(body)
+        })
+})
+
+router.patch('/books/:id', (req, res)=>{
+    const id = req.params.id
+    const url = 'http://192.168.56.102/books/'+id
+    request({url, json:true, method: 'PATCH', body: req.body}, (error,  {body})=>{
+            if(error){
+               return res.status(404).send(error)
+           }
+           return res.status(200).send(body)
+        })
+})
+
+router.use('/books/purchase/:id', (req, res)=>{
+    const id = req.params.id
+    const url = 'http://192.168.56.101/books/purchase/'+id
+    request({url, json:true}, (error, {body})=>{
+        if(error){
+            return res.send(error)
+        }
+        return res.status(200).body
+    })
+})
+
+module.exports = router 
