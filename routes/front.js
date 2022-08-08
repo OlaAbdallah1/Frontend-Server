4a1c975e3503510a274dfa755c8a1a7e1da25195
Ola Abdallah <67606039+OlaAbdallah1@users.noreply.github.com>
2022-08-08 14:17:33 +0300

    Added Load balancing and caching

--- routes/front.js
+++ routes/front.js
@@ -4,8 +4,8 @@ const router = express.Router()
 
 router.use('/books/info/:id', async(req, res)=>{
     const id = req.params.id
-    const url = 'http://192.168.56.102/books/info/'+id
-    request({url, json:true}, (error,  {body})=>{
+    const url = 'http://192.168.56.102:3008/books/info/'+id
+    request({url, json:true}, (error,  {})=>{
            if(error){
                return res.staus(404).send(error)
            }
@@ -14,7 +14,7 @@ router.use('/books/info/:id', async(req, res)=>{
 })
 router.use('/books/search/:topic', (req, res)=>{
     const topic = req.params.topic
-    const url = 'http://192.168.56.102/books/search/'+topic
+    const url = 'http://192.168.56.102:3007/books/search/'+topic
     request({url, json:true}, (error,  {body})=>{
            if(error){
                return res.staus(404).send(error)
@@ -25,7 +25,7 @@ router.use('/books/search/:topic', (req, res)=>{
 
 router.patch('/books/:id', (req, res)=>{
     const id = req.params.id
-    const url = 'http://192.168.56.102/books/'+id
+    const url = 'http://192.168.56.102:3007/books/'+id
     request({url, json:true, method: 'PATCH', body: req.body}, (error,  {body})=>{
             if(error){
                return res.status(404).send(error)
@@ -36,7 +36,7 @@ router.patch('/books/:id', (req, res)=>{
 
 router.use('/books/purchase/:id', (req, res)=>{
     const id = req.params.id
-    const url = 'http://192.168.56.101/books/purchase/'+id
+    const url = 'http://192.168.56.101:3008/books/purchase/'+id
     request({url, json:true}, (error, {body})=>{
         if(error){
             return res.send(error)
