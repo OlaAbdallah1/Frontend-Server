aef2b5146d05d9f0e1e730829886fa1475eb043f
Ola Abdallah <67606039+OlaAbdallah1@users.noreply.github.com>
2022-08-08 15:21:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const frontRouter = require('./routes/front.js')
+const logPort = require('./middlewares/logPort')
 
 const app = express()
 const PORT = process.env.PORT || 3002
 
 app.use(express.json())
 app.use(frontRouter)
+app.use(logPort)
 
 app.listen(PORT, ()=>{
-    console.log('Front server is running on port:'+ PORT)
+    console.log('Front server is up and running on port:'+ PORT)
 }) 
